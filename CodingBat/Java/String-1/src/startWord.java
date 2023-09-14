d01fa34eb442fe2134e0b2494bd78dcc89703b6a
Febriyan Naufal <69071485+novaru@users.noreply.github.com>
2023-09-15 04:50:40 +0700

    Create startWord.java

---
+++ CodingBat/Java/String-1/src/startWord.java
@@ -0,0 +1,10 @@
+public String startWord(String str, String word) {
+  if (str.length() >= word.length())
+  {
+    if (str.substring(1, word.length()).equals(word.substring(1)))
+    {
+      return str.substring(0, word.length());
+    }
+  }
+  return "";
+}
